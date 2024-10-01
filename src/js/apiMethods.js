05a5f1434e53067223fc77f154737fae2af1e7f6
Mohamed Ibrahim <mohamedhassen.ibrahim@mail.utoronto.ca>
2024-10-01 19:20:01 -0400

    Reworked form validation

--- src/js/apiMethods.js
+++ src/js/apiMethods.js
@@ -1,10 +1,6 @@
+import { domHandler } from "./domMethods";
+
 export const apiHandler = (function (){
-  function weatherData(location, temperature) {
-    return {
-      location,
-      temperature,
-    }
-  }
 
   function processUrl(location) {
     const locationParts = location.split(" ");
@@ -27,11 +23,13 @@ export const apiHandler = (function (){
     try {
       const url = processUrl(location);
       const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${url}?unitGroup=metric&include=current&key=MUA2C8Z3Z62WNA7QZU3KXWHN4&contentType=json`, {mode: 'cors'});
+      
+      
       const result = await response.json();
       const formattedResult = extractWeatherData(result);
       return formattedResult;
     } catch (error) {
-      console.error('There was an error fetching the data:', error);
+        domHandler.showError();
     }
   }
   
--- src/js/domMethods.js
+++ src/js/domMethods.js
@@ -1,14 +1,39 @@
 import { apiHandler } from "./apiMethods";
 
 export const domHandler = (function(){
-  const submitButton = document.querySelector("#search-button");
+  const locationInput = document.querySelector("#location");
+  const locationRequest = document.querySelector("form");
+  const errorText = document.querySelector(".error");
 
-  function getLocationFromForm() {
-    const searchBox = document.querySelector("#location");
-    const location = searchBox.value;
-    const weatherData = apiHandler.getWeatherData(location);
-    weatherData.then(result => console.log(result)) 
+  function changeWeatherText(weatherData) {
+    console.log(weatherData);
   }
 
-  submitButton.addEventListener("click", getLocationFromForm);
+  function showError() {
+    if(locationInput.validity.valid){
+      errorText.textContent = "Location was not found!";
+    } else {
+      errorText.textContent = "";
+    }
+  }
+
+  function getLocationWeatherData(event) {
+    event.preventDefault();
+
+    if(!locationInput.validity.valueMissing){
+      errorText.textContent = "";
+      const location = locationInput.value;
+      const weatherData = apiHandler.getWeatherData(location);
+      weatherData.then(result => changeWeatherText(result));
+    } else {
+      showError();
+    }
+
+  }
+
+  locationRequest.addEventListener("submit", getLocationWeatherData);
+
+  return {
+    showError
+  }
 })();
